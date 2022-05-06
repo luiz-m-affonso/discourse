1d864a34d11bc44e14a40c2218a071e7e7ca829e
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-05-06 21:35:29 +0400

    Bind status to template, disable the Save button when status isn't set

--- app/assets/javascripts/discourse/app/controllers/user-status.js
+++ app/assets/javascripts/discourse/app/controllers/user-status.js
@@ -2,11 +2,15 @@ import Controller from "@ember/controller";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
 import { action } from "@ember/object";
 import bootbox from "bootbox";
+import { notEmpty } from "@ember/object/computed";
 
 export default Controller.extend(ModalFunctionality, {
+  userStatus: "",
+  statusIsSet: notEmpty("userStatus"),
+
   @action
   saveAndClose() {
     this.send("closeModal");
-    bootbox.alert("Status is set.");
+    bootbox.alert(`The new status is "${this.userStatus}"`);
   },
 });
