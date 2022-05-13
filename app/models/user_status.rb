a5962adc56a5f99adc541013ffa3c66cd4291149
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-05-13 19:14:20 +0400

    Validation

--- app/models/user_status.rb
+++ app/models/user_status.rb
@@ -2,4 +2,12 @@
 
 class UserStatus < ActiveRecord::Base
   belongs_to :user
+
+  validate :ends_at_is_in_future
+
+  def ends_at_is_in_future
+    if ends_at && set_at > ends_at
+      errors.add(:ends_at, I18n.t("user_status.errors.ends_at_should_be_in_the_future"))
+    end
+  end
 end
